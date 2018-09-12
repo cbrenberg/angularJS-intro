0aa01db1b8fd0ae565c2065e15811ee343a5584c
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-12 13:43:04 -0500

    append array items to DOM, show and hide conditionally

--- client.js
+++ client.js
@@ -1,1 +1,19 @@
 console.log('client.js');
+
+let myApp = angular.module('MyApp', []);
+
+myApp.controller('WelcomeController', function(){
+  let self = this;
+
+  self.message = '';
+  self.addPointyThing = function(input){
+    self.pointyThings.push(input);
+  }
+  self.pointyThings = [
+    {name: 'knives', sharpness: 10},
+    {name: 'wits', sharpness: 11},
+    {name:'waffle cones', sharpness: 3},
+    {name: 'sea urchins', sharpness: 5, isPoisonous: true},
+    {name:'narwhals', sharpness: 7}];
+})
+
